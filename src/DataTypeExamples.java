7e2f44ab619394116472d4ba8550022bdcd9bb8b
amolu <amolujagare@gmail.com>
2023-08-16 20:41:57 +0530

    Initial commit

---
+++ src/DataTypeExamples.java
@@ -0,0 +1,27 @@
+public class DataTypeExamples {
+
+    public static void main(String[] args) {
+
+        byte b = -128; // -128 to 127
+        short s = -32768; // -32768 to 32767
+        int i = 2147483647; //-2147483648 to 2147483647
+        long l = 9223372036854775807l; // -9223372036854775808 to 9223372036854775807
+
+        float f = 12.34f;
+        double d = 23.45;
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        char ch = 'j';
+
+         char ch1 = 45 ;
+
+        System.out.println(ch1);
+
+         int i1 = 'f';
+
+        System.out.println(i1);
+
+    }
+}
